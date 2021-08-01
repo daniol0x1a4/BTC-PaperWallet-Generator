a242c6836b74a2f6ea550970bc5b997b8322a9db
Daniel <87243735+danielinfosec@users.noreply.github.com>
2021-08-01 14:36:46 +0200

    Update btcpaperwallet.py

--- btcpaperwallet.py
+++ btcpaperwallet.py
@@ -8,13 +8,14 @@ import qrcode
 
 #(change out basekey value to create new wallet)
 spaces = "----------------------------------------------"
-basekey = "PUT IN YOUR RANDOM CHARACTERS FOR NEW WALLET ADRESS IN HERE"
+letters = string.ascii_letters + string.digits + string.ascii_lowercase + string.punctuation
+crypto =  ''.join(random.choice(letters) for i in range(10000)) 
 
 #PrivateKey
 print(spaces)
 key1 = "PrivateKey"
 print(key1)
-priv = sha256 (basekey)
+priv = sha256 (crypto)
 print(priv)
 print(spaces)
 
@@ -28,16 +29,10 @@ addr = pubtoaddr(pub)
 print(addr)
 print(spaces)
 
-#PublicKey (Wallet) QRCode Adress
 img = qrcode.make(addr)
 img.save("publicwalletqr.png")
-print("WalletAdress QR Code saved as publicwalletqr.png")
-print(spaces)
 
-#PrivateKey QRCode
 img = qrcode.make(priv)
 img.save("privatekey-qrcode.png")
-print("PrivateKey QR Code saved as privatekey-qrcode.png")
-print(spaces)
 
 
